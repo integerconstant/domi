51bd1f8085e5b595bcd93954a687a5cce655be80
Domi <Sir_Mountain@t-online.de>
2017-11-25 16:26:48 +0100

    Hallo

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,22 @@
-public class Main {
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
 
     public static void main (String[] args) {
+        launch();
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Group group = new Group();
+        Scene scene = new Scene(group);
+
+        stage.setScene(scene);
+
+        stage.show();
 
     }
 }
